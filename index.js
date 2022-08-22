c1232268263f32dbc49336f0bceee987621d3678
Christian Jones <c529j466@yahoo.com>
2022-08-22 02:27:44 -0500

    reseting classes and index.js

--- index.js
+++ index.js
@@ -1,26 +1,9 @@
-// Linking in fileshare and inquirer
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const fs = require("fs");
 
-const fs = require('fs');
-const inquirer = require('inquirer');
-const Employee = require('./lib/Employee')
-const Intern = require('./lib/Intern.js');
-const Manager = require('./lib/Manager.js');
-const Engineer = require('./lib/Engineer');
-
-inquirer
-.prompt([
-    {
-      type: 'confirm',
-      message: 'Would you like to create a team page?',
-      name: 'opener',
-    }
-])
-.then((response)=> {
-if (response.opener) {
-    console.log('ohno');
-    Manager.managerInfo();
-}
-})
 
 
 
