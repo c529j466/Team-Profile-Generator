2d9118e82f509715d0f174f44bb6e809604b6408
Christian Jones <c529j466@yahoo.com>
2022-08-23 03:45:07 -0500

    adjusted functionality and started the html rendering

--- index.js
+++ index.js
@@ -66,37 +66,99 @@ const createTeam = function() {
         ])
 
         .then((newResponse) => {
-          if (response.role === "Manager") {
-            const manager = new Manager(response.name, response.id, response.email, response.role, response.officeNumber);
+            if (response.role === "Manager") {
+            var manager = new Manager(response.name, response.id, response.email, response.role, response.officeNumber);
             teamMembers.push(manager);
+            ManagerHtml =  
+            `<div class="card shadow" style="width: 18rem;">
+            <div class="card-header bg-primary text-white">
+            <h2 class="card-title">${response.name}</h2>
+            <h3 class="card-title"><i class="fas fa-mug-hot mr-2">${response.role}</i></h3>
+        </div>
+        <div class="card-body bg-light">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${response.id}</li>
+                <li class="list-group-item">Email: <a href="mailto:${response.email}"> ${response.email}</a></li>
+                <li class="list-group-item">Office Number: ${newResponse.officeNumber}</li>
+            </ul>
+        </div>
+    </div>
+    </div>`
           }
 
           else if (response.role === "Engineer") {
             const engineer = new Engineer(response.name, response.id, response.email, response.role, newResponse.github);
             teamMembers.push(engineer);
+            EngineerHtml =  
+            `<div class="card shadow" style="width: 18rem;">
+            <div class="card-header bg-primary text-white">
+            <h2 class="card-title">${response.name}</h2>
+            <h3 class="card-title"><i class="fas fa-glasses mr-2">${response.role}</i></h3>
+        </div>
+        <div class="card-body bg-light">
+            <ul class="list-group">
+                <li class="list-group-item">ID: ${response.id}</li>
+                <li class="list-group-item">Email: <a href="mailto:${response.email}"> ${response.email}</a></li>
+                <li class="list-group-item">GitHub:<a href="https://github.com/${newResponse.github}" target="_blank" rel="noopener noreferrer"> ${newResponse.github}</a></li>
+            </ul>
+        </div>
+    </div>
+    </div>`
           }
 
            else if (response.role === "Intern") {
             const intern = new Intern(response.name, response.id, response.email, response.role, newResponse.school);
             teamMembers.push(intern);
           }
-          
           if (newResponse.addMember) {
             createTeam();
           } 
           
           else {
-            teamMembers.forEach((members) => {
-                console.log(members);
-            })
+
+        var starterHtml = 
+        `<!DOCTYPE html>
+        <html lang="en">
+
+        <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta http-equiv="X-UA-Compatible" content="ie=edge">
+        <meta name="Description" content="Enter your description here" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.5.0/css/bootstrap.min.css">
+        <link rel="stylesheet" href="style.css">
+        <script src="https://kit.fontawesome.com/c502137733.js"></script>
+        
+        <title>Team Generator</title>
+        </head>
+
+        <body>
+
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="col-12 jumbotron team-heading bg-danger text-white">
+                        <h1 class="text-center">My Team</h1>
+                    </div>
+                </div>
+            </div>
+            <div class="container">
+                <div class="row">
+                    <div class="team-area col-12 d-flex justify-content-center">
+                    </div>
+                </div>
+            </div>`
+
+            var HTML = starterHtml += ManagerHtml += EngineerHtml;
+
+
+            fs.writeFile('Team_Generator.HTML', HTML, (err) => {
+                if (err) {
+                  throw err;
+                }
+                console.log("Success!");
+                });
             };
             
-    fs.writeFile('Team_Generator.HTML', 'comming soon template literal variable', (err) => {
-        if (err) {
-          throw err;
-        }
-        console.log("Success!");
-        });
       });
     })
     .catch((err) => {
